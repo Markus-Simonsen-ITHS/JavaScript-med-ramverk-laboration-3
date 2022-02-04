3a21f1c4128fcfe492916ca81bac57bbdf4fe98e
vikiorf <viktor.johansson96@gmail.com>
2022-02-04 11:33:30 +0100

    fixed router

--- deg/src/router.js
+++ deg/src/router.js
@@ -1,21 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-import AboutView from './views/AboutView.vue'
-import ContactView from './views/ContactView.vue'
 import HomeView from './views/HomeView.vue'
 import LoginView from './views/LoginView.vue'
 
 export default createRouter({
   history: createWebHashHistory(),
   routes: [
-    {
-      component: AboutView,
-      path: '/about'
-    },
-    {
-      component: ContactView,
-      path: '/contact'
-    },
     {
       component: HomeView,
       path: '/'
