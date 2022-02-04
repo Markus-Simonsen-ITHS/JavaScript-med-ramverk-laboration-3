e994f5091925443b3dad94a94fb5446bbb310b5c
vikiorf <viktor.johansson96@gmail.com>
2022-02-04 11:58:17 +0100

    added html outline for homeview

--- deg/src/views/HomeView.vue
+++ deg/src/views/HomeView.vue
@@ -1,1 +1,57 @@
-<template><h1>Hem</h1></template>
+<script>
+  export default {}
+</script>
+
+<template>
+  <div>
+    <p>Matkonto</p>
+    <p>2500kr</p>
+    <p>----------</p>
+    <span>Budget: 4000 kr</span>
+  </div>
+  <div>
+    <p>Intäkter</p>
+    <p>+5000</p>
+  </div>
+  <div>
+    <p>Utgifter</p>
+    <p>-1500</p>
+  </div>
+  <div>
+    <h1>Pengar kvar</h1>
+    <router-link to="/history">Historik</router-link>
+  </div>
+  <ul class="category-list">
+    <li class="expense-category">
+      <div class="category-img">
+        <img src="../../assets/fox.jpeg" alt="Fox" />
+      </div>
+      <p>Mat</p>
+      <p>500 kr</p>
+      <p>Budget</p>
+      <p>2000 kr</p>
+    </li>
+    <li class="expense-category">
+      <button>Add</button>
+      <p>Lägg till</p>
+    </li>
+  </ul>
+</template>
+
+<style>
+  .category-list {
+    padding: 0;
+    display: grid;
+  }
+  .expense-category {
+    list-style-type: none;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  .category-img {
+    grid-row: span 2;
+  }
+  img {
+    width: 100%;
+  }
+</style>
