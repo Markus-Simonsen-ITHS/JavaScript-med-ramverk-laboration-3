f01a102440a8cf9a102efc1bb56b5820d083c0bc
lobosestrada <marcela.araneda.salas@iths.se>
2022-02-07 19:20:18 +0100

    add transaction page

--- deg/src/views/TransactionsView.vue
+++ deg/src/views/TransactionsView.vue
@@ -13,43 +13,43 @@
       <h2>TITEL</h2>
       <div class="transaction-budget">
         <h5>BUDGET</h5>
-        <p>SUMMA</p>
+        <p>belopp</p>
       </div>
       <div class="transaction-text">
         <h4>INFO</h4>
-        <p>UTGIFT i kr</p>
+        <p>utgift i kr</p>
       </div>
     </div>
     <div class="transaction-card">
       <h2>TITEL</h2>
       <div class="transaction-budget">
         <h5>BUDGET</h5>
-        <p>BELOPP</p>
+        <p>belopp</p>
       </div>
       <div class="transaction-text">
         <h4>INFO</h4>
-        <p>UTGIFT i kr</p>
+        <p>utgift i kr</p>
       </div>
     </div>
     <div class="transaction-card">
       <h2>TITEL</h2>
       <div class="transaction-budget">
         <h5>BUDGET</h5>
-        <p>BELOPP</p>
+        <p>belopp</p>
       </div>
     </div>
     <div class="transaction-card">
       <h2>TITEL</h2>
       <div class="transaction-budget">
         <h5>BUDGET</h5>
-        <p>BELOPP</p>
+        <p>belopp</p>
       </div>
     </div>
     <div class="transaction-card">
       <h2>TITEL</h2>
       <div class="transaction-budget">
         <h5>BUDGET</h5>
-        <p>BELOPP</p>
+        <p>belopp</p>
       </div>
     </div>
   </div>
@@ -68,6 +68,12 @@
     background-color: #e7e7e7;
     margin: 2%;
     padding: 2%;
+    max-height: 244px;
+    max-width: 445px;
+    left: 33px;
+    top: 149px;
+    border-radius: 8px;
+
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   }
 
