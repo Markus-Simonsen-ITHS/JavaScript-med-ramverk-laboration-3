4236d3a9aaf11ba01e6bfde67e4274dcb656cb26
lobosestrada <marcela.araneda.salas@iths.se>
2022-02-15 13:49:45 +0100

    template in process

--- deg/src/views/TransactionsView.vue
+++ deg/src/views/TransactionsView.vue
@@ -46,12 +46,8 @@
     class="view-expenses"
     v-if="this.expenses === '' || this.expenses === null"
   >
-    <form class="expenses-form">
-      <div class="form-inner">
-        <h1>Inga utgifter har registrerats</h1>
-        <input type="button" value="Lägg till utgift" @click="goToAddView" />
-      </div>
-    </form>
+    <h1>Inga utgifter har registrerats</h1>
+    <input type="button" value="Lägg till utgift" @click="goToAddView" />
   </div>
 
   <!--Show if there are any registered transactions-->
@@ -72,6 +68,7 @@
             {{ expense.title }}
           </h4>
           <p v-for="expense in expenses" :key="expense">{{ expense.amount }}</p>
+          <p v-for="expense in expenses" :key="expense">{{ expense.date }}</p>
         </div>
       </div>
     </div>
@@ -79,43 +76,8 @@
 
   <ChartComp :data="expenses" />
 </template>
-
 <style scoped>
   * {
-    margin: 0;
-    padding: 0;
     box-sizing: border-box;
-  }
-
-  .view-expenses {
-    display: flex;
-    justify-content: center;
-    min-height: 100vh;
-    align-items: center;
-  }
-
-  .expenses-form {
-    display: block;
-    width: 100%;
-    padding: 15px;
-  }
-
-  .form-inner {
-    padding: 50px 15px;
-  }
-
-  .expenses-card {
-    display: flex;
-    flex-direction: column;
-    background-color: #e7e7e7;
-    margin: 2%;
-    padding: 2%;
-    max-height: 244px;
-    max-width: 445px;
-    left: 33px;
-    top: 149px;
-    border-radius: 8px;
-
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   }
 </style>
