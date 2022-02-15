18ef3d8e24c7542082093056e67044ec5c9003d0
lobosestrada <marcela.araneda.salas@iths.se>
2022-02-15 14:23:19 +0100

    test

--- deg/src/views/TransactionsView.vue
+++ deg/src/views/TransactionsView.vue
@@ -7,7 +7,7 @@
   export default {
     components: {
       NavBar,
-      ChartComp,
+      ChartComp
     },
     methods: {
       goToAddView() {
@@ -26,16 +26,16 @@
       goToChart() {
         const viewChart = true
         this.chart = viewChart
-      },
+      }
     },
     mounted() {
       this.fetchexpense()
     },
     data() {
       return {
-        expenses: [],
+        expenses: []
       }
-    },
+    }
   }
 </script>
 
@@ -46,8 +46,12 @@
     class="view-expenses"
     v-if="this.expenses === '' || this.expenses === null"
   >
-    <h1>Inga utgifter har registrerats</h1>
-    <input type="button" value="Lägg till utgift" @click="goToAddView" />
+    <form class="expenses-form">
+      <div class="form-inner">
+        <h1>Inga utgifter har registrerats</h1>
+        <input type="button" value="Lägg till utgift" @click="goToAddView" />
+      </div>
+    </form>
   </div>
 
   <!--Show if there are any registered transactions-->
@@ -60,7 +64,7 @@
     </ul>
     <div class="expenses-container">
       <div class="expenses-card">
-        <h2 v-for="expense in expenses" :key="expense">
+        <h2 v-for="expense in expenses" :key="expense.id">
           {{ expense.category }}
         </h2>
         <div class="expenses-text">
@@ -68,7 +72,6 @@
             {{ expense.title }}
           </h4>
           <p v-for="expense in expenses" :key="expense">{{ expense.amount }}</p>
-          <p v-for="expense in expenses" :key="expense">{{ expense.date }}</p>
         </div>
       </div>
     </div>
@@ -76,8 +79,43 @@
 
   <ChartComp :data="expenses" />
 </template>
+
 <style scoped>
   * {
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
+  }
+
+  .view-expenses {
+    display: flex;
+    justify-content: center;
+    min-height: 100vh;
+    align-items: center;
+  }
+
+  .expenses-form {
+    display: block;
+    width: 100%;
+    padding: 15px;
+  }
+
+  .form-inner {
+    padding: 50px 15px;
+  }
+
+  .expenses-card {
+    display: flex;
+    flex-direction: column;
+    background-color: #e7e7e7;
+    margin: 2%;
+    padding: 2%;
+    max-height: 244px;
+    max-width: 445px;
+    left: 33px;
+    top: 149px;
+    border-radius: 8px;
+
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   }
 </style>
