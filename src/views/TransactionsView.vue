210ccdc6f1016b76428b30b24506635050a56a97
lobosestrada <marcela.araneda.salas@iths.se>
2022-02-21 20:05:33 +0100

    merge conflict in TransactionsView

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -78,6 +78,10 @@
     padding: 10px;
     border-radius: 8px;
     background-color: #e7e7e7;
+    cursor: pointer;
+    margin: 30px;
+    padding: 10px;
+    border-radius: 8px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   }
 
