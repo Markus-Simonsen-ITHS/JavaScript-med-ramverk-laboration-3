50a9c36c735fa3d33bf3d611a2d82bc4c242b355
lobosestrada <marcela.araneda.salas@iths.se>
2022-02-22 16:05:19 +0100

    solved merge conflict in TransactionsView

--- src/views/TransactionsView.vue
+++ src/views/TransactionsView.vue
@@ -33,7 +33,6 @@
       v-for="budget in budgets"
       :key="budget"
     >
-<<<<<<< HEAD
       <img src="../../assets/fox.jpeg" alt="deg logo" />
       <p class="budget-title">{{ budget.title }}</p>
       <p class="budget-sum">budget:</p>
@@ -47,20 +46,6 @@
         <p class="expense-title" v-show="toggle">
           {{ expense.title }}
         </p>
-=======
-      <div v-for="item in budget.items" :key="item">
-        <img src="../../assets/fox.jpeg" alt="deg logo" />
-        <p class="expense-category">
-          {{ expense.category }}
-        </p>
-        <p class="budget-title">
-          {{ budget.title }}
-        </p>
-        <p class="budget-sum">{{ budget.sum }} kr</p>
-      </div>
-      <div v-for="expense in expenses" :key="expense">
-        <p class="expense-title" v-show="toggle">{{ expense.title }}</p>
->>>>>>> d095c91 (working on one-card display for each category)
         <p class="expense-amount" v-show="toggle">{{ expense.amount }} kr</p>
       </div>
     </div>
@@ -80,7 +65,6 @@
     height: 100%;
     width: 100%;
     max-width: 547px;
-<<<<<<< HEAD
   }
 
   .history-list {
@@ -99,82 +83,6 @@
     padding: 10px;
     border-radius: 8px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-=======
-  }
-
-  .history-list {
-    flex-direction: column;
-    list-style: none;
-    cursor: pointer;
-    margin: 30px;
-    padding: 10px;
-    border-radius: 8px;
-    background-color: #e7e7e7;
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-  }
-
-  .history-list,
-  p {
-    display: flex;
-    line-height: 0.5;
-    letter-spacing: 0.5px;
-    font-size: 1rem;
-    text-transform: capitalize;
-  }
-
-  img {
-    width: 56px;
-    height: 56px;
-    left: 44px;
-    top: 160px;
-    border-radius: 8px;
-  }
-
-  .expense-category {
-    padding: 0 1em 0 0;
-    font-weight: bold;
-    font-size: 1.4em;
-    margin-top: -30px;
-    margin-left: 65px;
-  }
-
-  .budget-sum {
-    align-self: flex-end;
-    margin-top: -24px;
-    margin-left: 65px;
-  }
-
-  .expense-title {
-    padding: 1rem 0 1em 0;
-    border-top: 1px solid black;
-  }
-
-  .expense-amount {
-    align-self: flex-end;
-    margin-top: -40px;
-    margin-left: 65px;
-  }
-
-  @media screen and (max-width: 500px) {
-    .history-list {
-      margin: 10px;
-      padding: 15px;
-      min-width: 200px;
-    }
-
-    img {
-      width: 45px;
-      height: 45px;
-    }
-
-    .expense-category {
-      padding: 0 1em 0 0;
-      font-weight: bold;
-      font-size: 1.2em;
-      margin-top: -30px;
-      margin-left: 55px;
-    }
->>>>>>> d095c91 (working on one-card display for each category)
   }
 
   img {
